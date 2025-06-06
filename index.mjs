f05e8b119743ebc6243f1f5e0b5e97e3a7528405
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2025-06-06 22:19:57 +0100

    index.mjs: also export middleware so documentation can add them to the API docs

--- index.mjs
+++ index.mjs
@@ -3,7 +3,14 @@
 import ServerRouter from './Server/Router.mjs';
 import ClientRouter from './Client/Router.mjs';
 
+import { middleware_catch_errors, middleware_log_requests, middleware_parse_json } from './Server.mjs';
+
 export {
 	ServerRouter,
-	ClientRouter
+	ClientRouter,
+	
+	// Middleware - ServerRouter only!
+	middleware_catch_errors,
+	middleware_log_requests,
+	middleware_parse_json
 };
